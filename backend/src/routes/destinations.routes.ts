c33d92de63349e5037e3688f08d531c847763b6f
Jean-Francois Pierre <e0429114@cmaisonneuve.qc.ca>
2026-07-05 19:50:37 -0400

    feat: Ajout de la fonction Post pour les infos du pays

--- backend/src/routes/destinations.routes.ts
+++ backend/src/routes/destinations.routes.ts
@@ -37,4 +37,38 @@ destRouter.get("/test-pays/:pays", async (req: Request, res: Response) => {
   }
 });
 
+destRouter.post(
+  "/importer/:pays",
+  authentificationJWT,
+  async (req: Request, res: Response) => {
+    try {
+      const nomPays = req.params.pays as string;
+
+      const { data } = await recupererInfosPays(nomPays);
+      if (!data?.objects || data.objects.length === 0) {
+        return res
+          .status(404)
+          .json({ erreur: "Impossible de récupérer les données de l'API" });
+      }
+
+      const pays = data.objects[0];
+
+      const infosSupp = await prisma.infosSuppPays.create({
+        data: {
+          countryCode: pays.codes?.alpha_3 ?? "N/A",
+          drapeau_emoji: pays.flag?.url_png ?? "",
+          capitale: pays.capitals?.[0]?.name ?? "Inconnue",
+          devise: pays.currencies?.[0]?.name ?? "Inconnue",
+          langages: pays.languages?.[0]?.name ?? "Inconnu",
+        },
+      });
+      return res.json(infosSupp);
+    } catch (e) {
+      if (axios.isAxiosError(e)) {
+        return res.status(500).json({ erreur: "Erreur de serveur." });
+      }
+    }
+  },
+);
+
 export default destRouter;
