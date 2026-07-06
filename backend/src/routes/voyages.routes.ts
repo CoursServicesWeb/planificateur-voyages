e0ebb33e0d062d8ab44fa03f41d6faddd93f22a8
Amit <a.chandel@pm.me>
2026-07-06 16:05:16 -0400

    CRUD Étapes
    - modification format du code

--- backend/src/routes/voyages.routes.ts
+++ backend/src/routes/voyages.routes.ts
@@ -6,13 +6,25 @@ import * as voyagesCtrls from '../controlleurs/voyages/voyagesControlleurs.js'
 
 const voyagesRouter = Router()
 
-voyagesRouter.get('/moi',authentificationJWT,niveauRequis("Voyageur"),voyagesCtrls.getVoyagesUtlisateur)
+voyagesRouter.get('/moi',
+    authentificationJWT,
+    niveauRequis("Voyageur"),
+    voyagesCtrls.getVoyagesUtlisateur)
 
-voyagesRouter.post('/',authentificationJWT,niveauRequis('Voyageur'),voyagesCtrls.creerVoyage)
+voyagesRouter.post('/',
+    authentificationJWT,
+    niveauRequis('Voyageur'),
+    voyagesCtrls.creerVoyage)
 
-voyagesRouter.patch('/:voyageid',authentificationJWT,niveauRequis('Voyageur'),voyagesCtrls.modifierVoyage)
+voyagesRouter.patch('/:voyageid',
+    authentificationJWT,
+    niveauRequis('Voyageur'),
+    voyagesCtrls.modifierVoyage)
 
-voyagesRouter.delete('/:voyageid',authentificationJWT,niveauRequis('Voyageur'),voyagesCtrls.supprimerVoyage)
+voyagesRouter.delete('/:voyageid',
+    authentificationJWT,
+    niveauRequis('Voyageur'),
+    voyagesCtrls.supprimerVoyage)
 
 export default voyagesRouter
 
