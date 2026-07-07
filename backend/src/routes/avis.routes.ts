ae2ab9ef5c6c1edf6b841a4a92629aca782987e2
ammour nadjib <nadjib.ammour@gmail.com>
2026-07-07 18:09:20 -0400

    refactor(routes): harmonisation des routes et prise en compte des retours

--- backend/src/routes/avis.routes.ts
+++ backend/src/routes/avis.routes.ts
@@ -1,42 +1,24 @@
 import { Router } from "express";
 
-import * as authMiddleware from '../middleware/auth.js';
-
-import * as avisControlleurs from '../controlleurs/avis/avisControlleurs.js';
+import * as authMiddleware from "../middleware/auth.js";
+import * as avisControlleurs from "../controlleurs/avis/avisControlleurs.js";
 
 const avisRouteur = Router();
 
-// ------ Récupérer tous les avis --------------- //
+// ------ Récupérer tous les avis ------ //
 
-avisRouteur.get('/',avisControlleurs.getAvis);
+avisRouteur.get(
+    "/",
+    avisControlleurs.getAvis
+);
 
-// ------ Récupérer un avis -------------------- //
+// ------ Ajouter un avis ------ //
 
-avisRouteur.get('/:id',avisControlleurs.getAvisById);
-
-// ------ Ajouter un avis --------------------- //
-
-avisRouteur.post('/',
+avisRouteur.post(
+    "/",
     authMiddleware.authentificationJWT,
-    authMiddleware.niveauRequis('Voyageur'),
+    authMiddleware.niveauRequis("Voyageur"),
     avisControlleurs.creerAvis
-);
-
-// ------ Modifier un avis -------------------- //
-avisRouteur.patch(
-    '/:id',
-    authMiddleware.authentificationJWT,
-    authMiddleware.niveauRequis('Voyageur'),
-    avisControlleurs.modifierAvis
-);
-
-// ------- Supprimer un avis ------------ //
-
-avisRouteur.delete(
-    '/:id',
-    authMiddleware.authentificationJWT,
-    authMiddleware.niveauRequis('Voyageur'),
-    avisControlleurs.supprimerAvis
 );
 
 export default avisRouteur;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,27 +1,35 @@
-import dotenv from "dotenv"
-import express, { type Request, type Response }  from 'express'
-import authRouter from "./routes/auth.routes.js"
-import etapesRouteur from "./routes/etapes.routes.js"
-import voyagesRouter from "./routes/voyages.routes.js"
+import dotenv from "dotenv";
+import express from "express";
 
-dotenv.config()
+import authRouter from "./routes/auth.routes.js";
+import voyagesRouter from "./routes/voyages.routes.js";
+import etapesRouteur from "./routes/etapes.routes.js";
+import destinationsRouteur from "./routes/destinations.routes.js";
+import avisRouteur from "./routes/avis.routes.js";
+
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use('/auth',authRouter);
-app.use('/voyages', voyagesRouter);
-app.use('/etapes', etapesRouteur);
-app.use('/destinations', destinationsRouteur);
-app.use('/avis', avisRouteur);
+// ---------- Authentification ---------------------//
+app.use("/auth", authRouter);
 
-app.use('/api/etapes',etapesRouteur)
-app.use('/api/voyages',voyagesRouter)
+//----------- Voyages -----------------------------//
+app.use("/voyages", voyagesRouter);
 
-app.listen(PORT, ()=> {console.log(`Serveur prêt sur port ${PORT}`)})
+// ---------- Étapes -----------------------------//
+app.use("/etapes", etapesRouteur);
 
+//----------- Destinations ----------------------//
+app.use("/destinations", destinationsRouteur);
 
+//----------- Avis -----------------------------//
+app.use("/avis", avisRouteur);
 
+app.listen(PORT, () => {
+    console.log(`Serveur prêt sur port ${PORT}`);
+});
