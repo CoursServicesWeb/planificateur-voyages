b04d96de8b2d375afaacc564bed8915d3fdcac62
Jean-Francois Pierre <e0429114@cmaisonneuve.qc.ca>
2026-07-07 18:24:45 -0400

    feat: Ajout de Ondelete: Cascade dans la table Avis pour les destinations

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,9 @@
 import dotenv from "dotenv";
 import express, { type Request, type Response } from "express";
 import authRouter from "./routes/auth.routes.js";
-import paysRouter from "./routes/pays.routes.js";
-import etapesRouter from "./routes/etapes.routes.js";
+import etapesRouteur from "./routes/etapes.routes.js";
 import voyagesRouter from "./routes/voyages.routes.js";
+import paysRouter from "./routes/pays.routes.js";
 import destinationsRouter from "./routes/destinations.routes.js";
 
 dotenv.config();
@@ -13,12 +13,12 @@ const app = express();
 
 app.use(express.json());
 
-// Routes
 app.use("/auth", authRouter);
-app.use("/api/pays", paysRouter); // N'oubliez pas d'enregistrer le nouveau routeur !
-app.use("/etapes", etapesRouter);
+
+app.use("/etapes", etapesRouteur);
 app.use("/voyages", voyagesRouter);
 app.use("/destinations", destinationsRouter);
+app.use("api/pays", paysRouter);
 
 app.listen(PORT, () => {
   console.log(`Serveur prêt sur port ${PORT}`);
