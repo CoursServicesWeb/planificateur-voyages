87abaefbecd17316d0612ed8506e57594a0286e5
Amit <a.chandel@pm.me>
2026-07-17 01:00:54 -0400

    Intégration de données météo:
    - ajout de données météo lors de la récupération des étapes

--- backend/src/api/meteoApi.ts
+++ backend/src/api/meteoApi.ts
@@ -5,3 +5,44 @@ export const meteoApi = axios.create({
     baseURL: "https://api.open-meteo.com/v1",
     timeout: 10000,
 });
+
+export const codesWMO: Record<number,string> = {
+    0:"ciel clair",
+    1:"ciel plutôt clair",
+    2:"ciel partiellement couvert",
+    3:"ciel couvert",
+    45:"brouillard",
+    48:"brouillard givrant",
+    51:"pluie fine légère",
+    53:"pluie fine moyenne",
+    55:"pluie fine dense",
+    56:"pluie fine givrante légère",
+    57:"pluie fine givrante dense",
+    61:"pluie légère",
+    63:"pluie moyenne",
+    65:"pluie intense",
+    66:"pluie verglaçante légère",
+    67:"pluie verglaçante intense",
+    71:"neige légère",
+    73:"neige moyenne",
+    75:"neige intense",
+    77:"neige en grains",
+    80:"averses de pluie légères",
+    81:"averses de pluie moyennes",
+    82:"averses de pluie violentes",
+    85:"averses de neige légères",
+    86:"averses de neige lourdes",
+    95:"orage",
+    96:"orage accompagnés de grêle légère",
+    99:"orage accompagnés de grêle intense"
+}
+
+export type Meteo = {
+    date: string;
+    temp_max: number,
+    temp_min: number,
+    sunrise: string,
+    sunset: string,
+    uv_index: number,
+    description: string
+}
--- backend/src/controlleurs/etapes/etapesControlleurs.ts
+++ backend/src/controlleurs/etapes/etapesControlleurs.ts
@@ -2,6 +2,8 @@ import { type Request, type Response } from "express";
 import prisma from '../../utils/prisma.js'
 import { Prisma } from '../../../generated/prisma/client.js'
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/client'
+import axios from 'axios'
+import { meteoApi, codesWMO, type Meteo } from "../../api/meteoApi.js";
 
 /**
  * @function ajouterEtape(req:Request,res:Response)
@@ -161,13 +163,82 @@ async function getEtapes(req:Request,res:Response) {
                 voyageId:req.params.voyageid as string
             }
         })
+        
+        const meteoParDestination: Map<number,Array<Array<Meteo>>> = new Map();
 
-        return res.status(200).json(etapes)
+        const promises = etapes.map(async etape =>{
+            try {
+                const destination = await prisma.destination.findUnique({
+                    where: {
+                        id:etape.destinationId
+                    },
+                    select: {
+                        id:true,
+                        ville:true,
+                        lat:true,
+                        long:true
+                    }
+                })
+
+                if (destination?.id && !(meteoParDestination.has(destination.id))) {
+                    meteoParDestination.set(destination?.id,[]);
+                }
+                
+                const diff = (etape.dateFin.getTime()-etape.dateDeb.getTime())/(24*60*60*1000);
+                
+                const { data } = await meteoApi.get('/forecast?',{
+                    params:{
+                        latitude:`${destination?.lat}`,
+                        longitude:`${destination?.long}`,
+                        daily:"weather_code,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,uv_index_max",
+                        timezone:"auto",
+                        start_date:`${etape.dateDeb.toISOString().split('T')[0]}`,
+                        end_date:  `${etape.dateFin.toISOString().split('T')[0]}`
+                    }
+                })
+
+                const datesPrevisionCopie: string[] = [...data.daily.time]
+
+                const previsionDestination: Meteo[] = new Array();
+
+                datesPrevisionCopie.forEach( () => {
+                    previsionDestination.push(
+                        {
+                            date: data.daily.time.shift(),
+                            temp_max: data.daily.apparent_temperature_max.shift(),
+                            temp_min: data.daily.apparent_temperature_min.shift(),
+                            sunrise: data.daily.sunrise.shift(),
+                            sunset: data.daily.sunset.shift(),
+                            uv_index: data.daily.uv_index_max.shift(),
+                            description: codesWMO[data.daily.weather_code.shift()] as any
+                        }
+                    )
+
+                })
+
+                meteoParDestination.get(destination!.id)?.push(previsionDestination)
+                return
+                
+            } catch(err){
+                throw err
+            }
+            
+        })
+
+        await Promise.all(promises)
+
+        const meteoParDestinationJSON = Object.fromEntries(meteoParDestination)
+        return res.status(200).json({ etapes, meteoParDestinationJSON })
+
     } catch(error){
         if (error instanceof Prisma.PrismaClientKnownRequestError) {
             return res.status(500).json({message:"Erreur lors de la recherche des étapes."})
         } else if(error instanceof Prisma.PrismaClientValidationError) {
             return res.status(400).json({message:"Une erreure est survenue lors de la recherche des étapes.  Veuillez vérifier la requête."})
+        } else if (axios.isAxiosError(error) && error.response) {
+            console.error("Erreur axios lors de la récupération des données météo: ", error.response.status);
+        } else {
+            console.error("Une erreur inconnue est survenue.")
         }
     }
 }
