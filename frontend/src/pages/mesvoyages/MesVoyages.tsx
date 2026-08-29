f1e004a8426227707f206190556cb33199c28fba
Amit <a.chandel@pm.me>
2026-08-29 11:51:25 -0400

    Ajout de type, code chargement des voyages, modification backend
    - ajout de dossier type/ dans src/ pour les types et interfaces locaux
    - ajout de user.ts dans src/type/ pour modifier étendre le type du payload JWT afin d'inclure le prénom de l'utilisateur.
    - modification du controlleur authConnexion dans le backend afin d'inclure le prénom dans le payload JWT

--- frontend/src/pages/mesvoyages/MesVoyages.tsx
+++ frontend/src/pages/mesvoyages/MesVoyages.tsx
@@ -1,13 +1,79 @@
-import CoreLayout from "../../components/layout/core/CoreLayout"
-import { useAuth } from "../../context/AuthContext"
 
-export default function MesVoyages() {
-    const { seDeconnecter } = useAuth();
-    seDeconnecter()
+import { useEffect, useState } from "react";
+import CoreLayout from "../../components/layout/core/CoreLayout";
+import {type Voyage } from '../../../../shared/types/voyage'
+import { type userData } from "../../../src/types/user";
+import { jwtDecode } from "jwt-decode";
+import { api } from "../../api/axios";
+import { VoyageCard } from "./components/VoyageCard";
+import { AjouterVoyage } from "./components/AjouterVoyage";
+
+
+export default function MesVoyages () {
+
+    const token = localStorage.getItem('token')
+
+    let userData : userData;
+    
+        if (token) {
+            try {
+                userData = jwtDecode(token);
+            } catch(error) {
+                console.error(error)
+            }
+        } 
+
+    const [voyages, setVoyages] = useState<Voyage[]>([]);
+    const [isLoading, setIsLoading] = useState<Boolean>(true);
+
+    useEffect(() => {
+        async function chargerVoyages(){
+            const resp = await api.get('/voyages/moi')
+            setVoyages(resp.data.voyages);
+        }
+
+        try {
+            chargerVoyages();
+            setIsLoading(false);
+        } catch(error) {
+            console.error(error)
+            setIsLoading(false);
+        }
+    },[voyages])
+
+    const handleDelete = async (id:string) => {
+        try {
+            await api.delete(`/voyages/${id}`)
+            setVoyages((prev) => prev.filter(v => v.id !== id))
+        
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
     return (
-        <CoreLayout >
-            <div className="container my-5">
-                Mes Voyages
+        <CoreLayout navUserName={userData!.given_name}>
+            <div className="container">
+                {isLoading ? (
+                    <p>Chargement des voyages....</p>
+                ) : (
+                    <div className="row">
+                        <div className="col-6 bg-light">
+                            {voyages.map(v => 
+                                <VoyageCard 
+                                    key={v.id} 
+                                    id={v.id} 
+                                    titre= {v.titre} 
+                                    handleCardClick = {()=>{}} 
+                                    deleteHandler={handleDelete}/>)}
+                                <AjouterVoyage/>
+                        </div>
+                        <div className="col-6 bg-light">
+                            Etapes
+                        </div>
+                    </div>
+                    )
+                }
             </div>
         </CoreLayout>
     )
---
+++ frontend/src/types/user.ts
@@ -0,0 +1,8 @@
+
+
+import { type JwtPayload } from "jwt-decode";
+
+export interface userData extends JwtPayload {
+    given_name : string;
+    role : string;
+}
