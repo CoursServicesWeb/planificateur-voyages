d65e2b06597f6a9f22002d082b0c02cbd9363985
FSDevastator <a.chandel@pm.me>
2026-09-02 15:29:55 -0400

    ModalAjouterVoyage - ajouté champs au modal pour ajouter un voyage, avec la fonctionalité de soumission en développement.

--- frontend/src/components/layout/core/Nav.tsx
+++ frontend/src/components/layout/core/Nav.tsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
 import type {Destination} from '../../../../../shared/types/destination'
 
-export default function Nav({ userName, lienVoyage, lienDestination, destData = null } : CoreNavProps) {
+export default function Nav({ userName, lienVoyage, lienDestination } : CoreNavProps) {
     return (
         <nav className="navbar-nav">
             <p>Bonjour {userName}</p>
             <Link to="/">{lienDestination}</Link>
-            <Link to="/mesvoyages" state = {destData ? {destData: destData } : null }>{lienVoyage}</Link>
+            <Link to="/mesvoyages">{lienVoyage}</Link>
         </nav>
     )
 }
